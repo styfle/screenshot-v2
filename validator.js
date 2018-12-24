350686939c018b3bbcd62a2ec87a1c9ac7af5a57
Steven <steven@ceriously.com>
2018-12-23 23:39:13 -0800

    Change to node-server builder

--- validator.js
+++ validator.js
@@ -1,8 +1,11 @@
+const { URL } = require('url');
+
 function isValidUrl(str) {
     try {
         const url = new URL(str);
         return url.hostname.includes('.');
     } catch(e) {
+        console.error(e.message);
         return false;
     }
 }
