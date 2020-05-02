2d5a1ce46d9ecccb0a6cb8be4a23fe0aaa89f118
Simon Willison <swillison@gmail.com>
2020-05-01 18:26:46 -0700

    Require auth header to call API (if AUTH_TOKEN is set)

--- screenshot.js
+++ screenshot.js
@@ -1,8 +1,15 @@
 const { parse } = require('url');
+const { timingSafeEqual } = require('crypto');
 const { getScreenshot } = require('./chromium');
 const { getInt, getUrlFromPath, isValidUrl } = require('./validator');
 
 module.exports = async function (req, res) {
+    if (process.env.AUTH_TOKEN && !timingSafeEqual(`Bearer ${process.env.AUTH_TOKEN}`, req.headers.authorization)) {
+        res.statusCode = 403;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Auth error</h1><p>You need to send the correct Authorization: header</p>');
+        return;
+    }
     try {
         const { pathname = '/', query = {} } = parse(req.url, true);
         const { type = 'png', quality, fullPage } = query;
